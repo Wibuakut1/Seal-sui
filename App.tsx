e937fa2edce48b1888f4634f4bb1b85219462180
Wibuakut1 <122853804+Wibuakut1@users.noreply.github.com>
2025-05-11 10:58:04 +0700

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="container fade-in">
+      <h1>Seal Example App</h1>
+      <p>Halo, ini adalah tampilan awal dari aplikasi Seal dengan desain modern dan tema gelap.</p>
+      <button onClick={() => alert('Halo dari Seal!')}>Klik Saya</button>
+    </div>
+  )
+}
+
+export default App
