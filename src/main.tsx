724d7c1bcc6351039cdda6e8f9c0b85f3d03be0d
Wibuakut1 <122853804+Wibuakut1@users.noreply.github.com>
2025-05-11 11:25:49 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -11,18 +11,20 @@ import { Theme } from '@radix-ui/themes';
 import App from './App';
 import { networkConfig } from './networkConfig';
 
+// Membuat instance dari QueryClient
 const queryClient = new QueryClient();
 
+// Render komponen ke dalam DOM
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Theme appearance="dark">
       <QueryClientProvider client={queryClient}>
         <SuiClientProvider networks={networkConfig} defaultNetwork="testnet">
           <WalletProvider autoConnect>
-            <App />
+            <App /> {/* Menambahkan App */}
           </WalletProvider>
         </SuiClientProvider>
       </QueryClientProvider>
     </Theme>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
